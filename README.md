d59b6deee43a77a8803e1febf504cdb5ca8ac6bd
Gustavo Henrique Orair <guru_gho@hotmail.com>
2020-06-17 03:25:54 -0300

    Primeira versão do scraper

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
 This is a scraper that runs on [Morph](https://morph.io). To get started [see the documentation](https://morph.io/documentation)
+
+## Projeto de Scraper dos Informes Diários dos Fundos de Investimentos do Brasil
+
+Este projeto objetiva principalmente resolver a necessidade de obtenção das informações das cotas dos fundos de investimento. Essas cotas são utilizadas para o acompanhamento do desempenho dos fundos.
+
+As cotas são obtidas diretamente do Portal de Dados Abertos da CVM:
+* http://dados.cvm.gov.br/ 
+
+Neste portal existem várias bases de dados sobre os fundos de investimentos. O foco deste projeto encontra-se nos informes diários:
+* Fundos de Investimento: Documentos: Informe Diário
+
+Caso não seja um desenvolvedor e não consiga utilizar com qualidade este scraper, sugere-se buscar uma solução com interface gráfica mais bem desenvolvida como o https://infofundos.com.br/.
+
+Esse projeto foi desenvolvido com o objetivo de encapsular a parte de obtenção dos dados e a utilização da API do morph.io para a obtenção das cotas.
+
+A documentação da API do Morph.io pode ser encontrada em:
+* https://morph.io/documentation/api
+
+Em particular, pretende-se utilizar a API do Morph.io para consumir os dados de dentro do Google SpreadSheets por meio do Google Apps script:
+* https://developers.google.com/apps-script
+
+## FORMATO DOS DADOS
+A documentação da CVM para este dataset pode ser obtida em: 
+* http://dados.cvm.gov.br/dataset/fi-doc-inf_diario
+
+### Documentação da CVM sobre o dataset
+O *INFORME DIÁRIO* é um demonstrativo que contém as seguintes informações do fundo, relativas à data de competência:
+* Valor total da carteira do fundo;
+* Patrimônio líquido;
+* Valor da cota;
+* Captações realizadas no dia;
+* Resgates pagos no dia;
+* Número de cotistas
+
+O conjunto de dados disponibiliza os informes diários referentes aos Fundos de Investimento nos últimos doze meses.
+
+Os arquivos referentes aos meses corrente (M) e anterior (M-1) serão atualizados diariamente com as eventuais reapresentações. A atualização ocorre de terça a sábado, às 08:00h, com os dados recebidos pelo CVMWeb até as 23:59h do dia anterior.
+
+Os arquivos referentes aos meses M-2, M-3, ..., até M-11 serão atualizados semanalmente com as eventuais reapresentações.
+
+Como o dicionário de dados pode ser atualizado pela CVM, sugere-se a consulta ao dicionário de dados que pode ser encontrado na página do dataset:
+* http://dados.cvm.gov.br/dados/FI/DOC/INF_DIARIO/META/meta_inf_diario_fi.txt
+
+## Decisões de projeto
+No projeto optou-se por obter as cotas a partir de 2018. 
+Para obter os dados de 2017 será necessário alterar a variável de ambiente SCRAPER_INFORME_CVM_ANO_INICIAL.
+
+Nota: Como os dados anteriores a 2017 estão compactados, este projeto não contemplou a obtenção destas informações anteriores a 2017.
+
+## Agendamento
+TODO: Deve-se definir uma política de agendamento e documentá-la
+
+## Variáveis de ambiente
+SCRAPER_INFORME_CVM_ANO_INICIAL
+default: 2018
+
+### Variáveis de ambiente do Morph.io
+Conforme a documentação do ScraperWiki utilizado pelo Morph.io:
+https://github.com/sensiblecodeio/scraperwiki-python
+
+SCRAPERWIKI_DATABASE_NAME
+default: scraperwiki.sqlite - name of database
+
+SCRAPERWIKI_DATABASE_TIMEOUT
+default: 300 - number of seconds database will wait for a lock
--- scraper.py
+++ scraper.py
@@ -1,24 +1,110 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+import os
+import datetime
+import click
+import scraperwiki
+import pandas as pd
+import urllib
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+@click.command("Gerenciador do Scraper dos Informes Diários de Fundos de Investimentos da CVM")
+@click.option('--ano_inicial', 
+                default=lambda: 
+                    os.environ.get('MORPHIO_INFORME_CVM_ANO_INICIAL', 2018), 
+                show_default="Variável de ambiente SCRAPER_INFORME_DIARIO_CVM_ANO_INICIAL ou o valor padrão 2018")
+@click.option('--skip_informacoes_cadastrais', 
+                default=False, is_flag=True, 
+                show_default=True)
+def executa_scraper(ano_inicial, skip_informacoes_cadastrais):
+    # TODO: Permitir mudar o database name
+    # Caso vá testar localmente utilizando a biblioteca padrão do scraper wiki,
+    # será necessário especificar a variável de ambiente para o valor padrão do morph.io
+    # os.environ['SCRAPERWIKI_DATABASE_NAME'] = 'sqlite:///data.sqlite'
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+    periodos=obtem_periodos(ano_inicial)
+    for periodo in periodos: 
+        print(f'Iniciando captura do arquivo de informe diário para o período {periodo}')
+        informe_diario_df=captura_arquivo(periodo)
+        # Verifica se recebeu os dados ok
+        if informe_diario_df is not None and not informe_diario_df.empty:
+            print(f'Salvando dados obtidos com {informe_diario_df.size} registros.')
+            salva_periodo(informe_diario_df, periodo)
+
+def captura_arquivo(periodo):
+    url = f'http://dados.cvm.gov.br/dados/FI/DOC/INF_DIARIO/DADOS/inf_diario_fi_{periodo}.csv'
+
+    try:
+        # Realiza a leitura dos dados como csv
+        # O delimitador dos csvs da CVM é o ';'
+        # Como os dados estão salvos em ISO-8859-1, 
+        # é necessário alterar o encoding durante a leitura dos dados
+        informe_diario_df = pd.read_csv(
+            url,
+            sep=';',
+            encoding='latin1'
+        )
+    except (IOError, urllib.error.HTTPError) as err:        
+        print('Falha na leitura do arquivo ', url ,'...', err)
+        print(type(err))    # the exception instance
+        print(err.args)     # arguments stored in .args
+        return None
+    except Exception as err:
+        print('Erro ao baixar arquivo', url, '...', err)
+        print(type(err))    # the exception instance
+        print(err.args)     # arguments stored in .args
+        return None
+
+    # Cria um campo só com os números do CNPJ
+    informe_diario_df['COD_CNPJ'] = informe_diario_df['CNPJ_FUNDO'].str.replace(r'\D+', '').str.zfill(14)
+    informe_diario_df['DT_REF'] = pd.to_datetime(informe_diario_df['DT_COMPTC'], errors='coerce').dt.strftime('%Y-%m-%d')
+    
+    return informe_diario_df;
+
+def obtem_periodos(ano_inicial=2018):
+    periodos=[]
+
+    today = datetime.date.today()
+    ano_final = int(today.strftime('%Y'))
+    mes_final = int(today.strftime('%m'))
+
+    for ano in range(ano_inicial, ano_final+1):
+        for mes in range(1,13):
+            # evita pegar anos futuros, visto que o arquivo ainda não existe
+            if ano == ano_final and mes > mes_final:
+                break
+
+            periodo=f'{ano:04}{mes:02}'.format(ano, mes)
+            periodos.append(periodo)
+
+    return periodos;
+
+def salva_periodo(informe_diario_df, periodo):
+    # scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
+    # 
+    # # An arbitrary query against the database
+    # scraperwiki.sql.select("* from data where 'name'='peter'")
+
+    # You don't have to do things with the ScraperWiki and lxml libraries.
+    # You can use whatever libraries you want: https://morph.io/documentation/python
+    # All that matters is that your final data is written to an SQLite database
+    # called "data.sqlite" in the current working directory which has at least a table
+    # called "data".
+
+    if informe_diario_df is None or informe_diario_df.empty:
+        print('Recebeu dados vazios!')
+        return False
+
+    try:
+        for row in informe_diario_df.to_dict('records'):
+            scraperwiki.sqlite.save(unique_keys=informe_diario_df.columns.values.tolist(), data=row, table_name='informe_diario')
+    except Exception as err:
+        print(f'Falha ao salvar registros no banco de dados para o período {periodo}', err)
+        print(type(err))    # the exception instance
+        print(err.args)     # arguments stored in .args
+        return None
+ 
+if __name__ == '__main__':
+    executa_scraper();
+
+    # Criar os índices do banco de dados
+    #create_command='CREATE [UNIQUE] INDEX index_name '
+    #              + 'ON table_name(column_list); '
+    # TODO: scraperwiki.sqlite.execute(sql[, vars], verbose=1)
